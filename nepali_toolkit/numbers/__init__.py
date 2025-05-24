abd8f5f1207b13a5b6f78295317afcd74382e73e
bibekjoshi01 <bibekjoshi34@gmail.com>
2025-05-24 11:15:41 +0545

    number words conversion

--- nepali_toolkit/numbers/__init__.py
+++ nepali_toolkit/numbers/__init__.py
@@ -1,7 +1,10 @@
-from .convert import to_nepali_number, to_english_number, is_nepali_number
+from .convert import *
+from .words import *
 
 __all__ = [
     "to_nepali_number",
     "to_english_number",
     "is_nepali_number",
+    "contains_nepali_digits",
+    "number_to_nepali_words",
 ]
